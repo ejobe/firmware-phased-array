b7d5c88b0f6d44d3c5326fa6b3b309e3023a1c05
ejobe <ejo@uchicago.edu>
2019-04-23 15:04:22 -0500

    stricter filter, v2.7

---
+++ rtl/filter_v2.vhd
@@ -0,0 +1,246 @@
+---------------------------------------------------------------------------------
+-- Univ. of Chicago  
+--    --KICP--
+--
+-- PROJECT:      phased-array trigger board
+-- FILE:         filter.vhd
+-- AUTHOR:       e.oberla
+-- EMAIL         ejo@uchicago.edu
+-- DATE:         3/2019, 
+--
+-- DESCRIPTION:  FIR high-pass for surface trigger
+--               
+---------------------------------------------------------------------------------
+library IEEE;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_unsigned.all;
+use ieee.numeric_std.all;
+
+use work.defs.all;
+
+entity filter_v2 is
+	port(
+		rst_i			:	in		std_logic;
+		clk_i			: 	in		std_logic;
+		clk_iface_i	:	in		std_logic;
+			
+		reg_i			: 	in		register_array_type;
+		data_i		:	in	   surface_data_type;
+		
+		filtered_data_o :	out	surface_data_type);
+		
+end filter_v2;
+
+architecture rtl of filter_v2 is
+
+type internal_buf_data_type is array (surface_channels-1 downto 0) of std_logic_vector(6*pdat_size-1 downto 0);
+signal dat : internal_buf_data_type;
+signal buf_data_0 		: 	surface_data_type;
+signal buf_data_1 		:  surface_data_type;
+signal buf_data_2 		: 	surface_data_type;
+signal buf_data_3 		: 	surface_data_type;
+signal buf_data_4 		: 	surface_data_type;
+signal buf_data_5 		: 	surface_data_type;
+signal wfm_data_pipe 	:  surface_data_type;
+
+constant filter_taps : integer :=  27;
+constant filter_coeff_size : integer :=  8; 
+constant filter_result_length : integer := 20; 
+
+-----------------------------------------------
+----filter via conversion to integer:
+---------------------------------------------
+type filter_kernel_type is array (0 to filter_taps-1) of integer range -127 to 128; --
+--constant kernel : filter_kernel_type := (-3,-4,-2,2,6,7,2,-8,-21,-31,96); --//F_c = 200MHz
+--constant kernel : filter_kernel_type := (-1,-4,-4,-1,5,8,5,-5,-19,-32,90); --//F_c = 220MHz
+--constant kernel : filter_kernel_type :=   (0,0,0,-1,-4,-4,-1,5,8,5,-5,-19,-32,90,-32,-19,-5,5,8,5,-1,-4,-4,-1,0,0,0); --//F_c = 220MHz [21 taps]
+--constant kernel : filter_kernel_type := (2,1,0,-2,-3,-2, 1,5,7,3,-7,-20,-31,93,-31,-20,-7,3,7,5, 1,-2,-3,-2,0,1,2); --//F_c = 250MHz [27 taps]
+--constant kernel : filter_kernel_type := (0,0,1, 0,-1,-2,-1,2,5,5,-3,-17,-31,90,-31,-17,-3,5,5,2,-1,-2,-1, 0,1,0,0); --//F_c = 230MHz triangle [27 taps]
+--constant kernel : filter_kernel_type := (0,0,0, 0,-1,-2,-1,2,6,5,-3, 18,-33,89,-33,-18,-3,5,6,2,-1,-2,-1, 0,0,0,0); --//F_c = 230MHz hamming[27 taps]
+--constant kernel : filter_kernel_type := ( 0,2,2,0,-2,-3,-2,3,7,6,-3,-19,-33,89,-33,-19,-3,6,7,3,-2,-3,-2,0,2,2, 0); --//equiripple : fstop=180, fpass=280
+constant kernel : filter_kernel_type := (-1,0,2,2, 0,-3,-4,0,6,8, 0,-17,-35,85,-35,-17, 0,8,6,0,-4,-3, 0,2,2,0,-1); --//equiripple : fstop=200, fpass=300
+
+-----------------------------------------------
+----filter via conversion to integer:
+-----------------------------------------------
+type filter_result_int_type is array (surface_channels-1 downto 0, 2*define_serdes_factor-1 downto 0) of integer range -4096 to 8192;
+signal filter_result : filter_result_int_type;
+signal pre_filter_result_1 : filter_result_int_type;
+signal pre_filter_result_2 : filter_result_int_type;
+signal pre_filter_result_3 : filter_result_int_type;
+signal pre_filter_result_4 : filter_result_int_type;
+signal pre_filter_result_5 : filter_result_int_type;
+
+signal buf_filter_result : surface_data_type;
+
+--//
+component signal_sync is
+port
+	(clkA			: in	std_logic;
+   clkB			: in	std_logic;
+   SignalIn_clkA	: in	std_logic;
+   SignalOut_clkB	: out	std_logic);
+end component;
+--//
+signal internal_filter_enable : std_logic := '0';
+--//
+begin
+--//
+--------------------------------------------
+xFILTEN : signal_sync
+port map(
+	clkA				=> clk_iface_i,
+	clkB				=> clk_i,
+	SignalIn_clkA	=> reg_i(47)(3), 
+	SignalOut_clkB	=> internal_filter_enable);
+--------------------------------------------
+
+proc_filter : process(rst_i, clk_i)
+begin
+	for j in 0 to surface_channels-1 loop
+
+		if rst_i = '1' then
+		
+			dat(j) <= (others=>'0');
+			buf_data_0(j) <= (others=>'0');
+			buf_data_1(j) <= (others=>'0');
+			buf_data_2(j) <= (others=>'0');
+			buf_data_3(j) <= (others=>'0');
+			
+			for i in 0 to 2*define_serdes_factor-1 loop
+				
+				-----------------------------------------------
+				----filter via conversion to integer:
+				-----------------------------------------------
+				filter_result(j,i) <= 0;
+				pre_filter_result_1(j,i) <= 0;
+				pre_filter_result_2(j,i) <= 0;
+
+			end loop;
+			
+	
+		elsif rising_edge(clk_i) then
+
+			dat(j) <= buf_data_0(j) & buf_data_1(j) & buf_data_2(j) & buf_data_3(j) & buf_data_4(j) & buf_data_5(j);
+
+			buf_data_5(j) <= buf_data_4(j);
+			buf_data_4(j) <= buf_data_3(j);	
+			buf_data_3(j) <= buf_data_2(j);	
+			buf_data_2(j) <= buf_data_1(j);	
+			buf_data_1(j) <= buf_data_0(j);
+			buf_data_0(j) <= wfm_data_pipe(j);
+			wfm_data_pipe(j) <= data_i(j);
+							  
+			case internal_filter_enable is
+				when '0' => filtered_data_o(j) <= buf_data_3(j);
+				when '1' => filtered_data_o(j) <= buf_filter_result(j);
+			end case;
+	
+		for i in 0 to 2*define_serdes_factor-1 loop
+
+		---------------------------------------------
+		--filter via conversion to integer:
+		---------------------------------------------
+		
+				buf_filter_result(j)((i+1)*define_word_size-1 downto i*define_word_size) <= '0' & 
+													std_logic_vector(to_unsigned((filter_result(j,i) + 4096), filter_result_length))(12 downto 6); --//get value	
+
+				--//--
+				filter_result(j,i) <= pre_filter_result_1(j,i) + pre_filter_result_2(j,i) + pre_filter_result_3(j,i) + pre_filter_result_4(j,i) + pre_filter_result_5(j,i);
+
+				--//--									
+				pre_filter_result_1(j,i) <=						
+					(to_integer(unsigned(dat(j)(3*pdat_size-2-define_word_size*0 + define_word_size*i downto 3*pdat_size-define_word_size*1 + define_word_size*i ))) - 64) *
+					kernel(0) + 
+				
+					(to_integer(unsigned(dat(j)(3*pdat_size-2-define_word_size*1 + define_word_size*i downto 3*pdat_size-define_word_size*2 + define_word_size*i ))) - 64) *
+					kernel(1) + 			
+
+					(to_integer(unsigned(dat(j)(3*pdat_size-2-define_word_size*2 + define_word_size*i downto 3*pdat_size-define_word_size*3 + define_word_size*i ))) - 64) *
+					kernel(2) + 	
+			
+					(to_integer(unsigned(dat(j)(3*pdat_size-2-define_word_size*3 + define_word_size*i downto 3*pdat_size-define_word_size*4 + define_word_size*i ))) - 64) *
+					kernel(3) + 		
+
+					(to_integer(unsigned(dat(j)(3*pdat_size-2-define_word_size*4 + define_word_size*i downto 3*pdat_size-define_word_size*5 + define_word_size*i ))) - 64) *
+					kernel(4);
+				
+				pre_filter_result_2(j,i) <=						
+					(to_integer(unsigned(dat(j)(3*pdat_size-2-define_word_size*5 + define_word_size*i downto 3*pdat_size-define_word_size*6 + define_word_size*i ))) - 64) *
+					kernel(5) + 
+					
+					(to_integer(unsigned(dat(j)(3*pdat_size-2-define_word_size*6 + define_word_size*i downto 3*pdat_size-define_word_size*7 + define_word_size*i ))) - 64) *
+					kernel(6) +
+					
+					(to_integer(unsigned(dat(j)(3*pdat_size-2-define_word_size*7 + define_word_size*i downto 3*pdat_size-define_word_size*8 + define_word_size*i ))) - 64) *
+					kernel(7) + 			
+
+					(to_integer(unsigned(dat(j)(3*pdat_size-2-define_word_size*8 + define_word_size*i downto 3*pdat_size-define_word_size*9 + define_word_size*i ))) - 64) *
+					kernel(8) + 	
+			
+					(to_integer(unsigned(dat(j)(3*pdat_size-2-define_word_size*9 + define_word_size*i downto 3*pdat_size-define_word_size*10 + define_word_size*i ))) - 64) *
+					kernel(9);
+					
+				pre_filter_result_3(j,i) <=
+					(to_integer(unsigned(dat(j)(3*pdat_size-2-define_word_size*10 + define_word_size*i downto 3*pdat_size-define_word_size*11 + define_word_size*i ))) - 64) *
+					kernel(10) +
+									
+					(to_integer(unsigned(dat(j)(3*pdat_size-2-define_word_size*11 + define_word_size*i downto 3*pdat_size-define_word_size*12 + define_word_size*i ))) - 64) *
+					kernel(11) +
+					
+					(to_integer(unsigned(dat(j)(3*pdat_size-2-define_word_size*12 + define_word_size*i downto 3*pdat_size-define_word_size*13 + define_word_size*i ))) - 64) *
+					kernel(12)+
+					
+					(to_integer(unsigned(dat(j)(3*pdat_size-2-define_word_size*13 + define_word_size*i downto 3*pdat_size-define_word_size*14 + define_word_size*i ))) - 64) *
+					kernel(13) +			
+
+					(to_integer(unsigned(dat(j)(3*pdat_size-2-define_word_size*14 + define_word_size*i downto 3*pdat_size-define_word_size*15 + define_word_size*i ))) - 64) *
+					kernel(14);
+			
+				pre_filter_result_4(j,i) <=	
+					(to_integer(unsigned(dat(j)(3*pdat_size-2-define_word_size*15 + define_word_size*i downto 3*pdat_size-define_word_size*16 + define_word_size*i ))) - 64) *
+					kernel(15) + 		
+
+					(to_integer(unsigned(dat(j)(3*pdat_size-2-define_word_size*16 + define_word_size*i downto 3*pdat_size-define_word_size*17 + define_word_size*i ))) - 64) *
+					kernel(16) +
+									
+					(to_integer(unsigned(dat(j)(3*pdat_size-2-define_word_size*17 + define_word_size*i downto 3*pdat_size-define_word_size*18 + define_word_size*i ))) - 64) *
+					kernel(17) + 
+					
+					(to_integer(unsigned(dat(j)(3*pdat_size-2-define_word_size*18 + define_word_size*i downto 3*pdat_size-define_word_size*19 + define_word_size*i ))) - 64) *
+					kernel(18) + 
+				
+					(to_integer(unsigned(dat(j)(3*pdat_size-2-define_word_size*19 + define_word_size*i downto 3*pdat_size-define_word_size*20 + define_word_size*i ))) - 64) *
+					kernel(19) + 			
+
+					(to_integer(unsigned(dat(j)(3*pdat_size-2-define_word_size*20 + define_word_size*i downto 3*pdat_size-define_word_size*21 + define_word_size*i ))) - 64) *
+					kernel(20);
+					
+				pre_filter_result_5(j,i) <=
+					(to_integer(unsigned(dat(j)(3*pdat_size-2-define_word_size*21 + define_word_size*i downto 3*pdat_size-define_word_size*22 + define_word_size*i ))) - 64) *
+					kernel(21) + 		
+
+					(to_integer(unsigned(dat(j)(3*pdat_size-2-define_word_size*22 + define_word_size*i downto 3*pdat_size-define_word_size*23 + define_word_size*i ))) - 64) *
+					kernel(22) +
+									
+					(to_integer(unsigned(dat(j)(3*pdat_size-2-define_word_size*23 + define_word_size*i downto 3*pdat_size-define_word_size*24 + define_word_size*i ))) - 64) *
+					kernel(23) + 
+					
+					(to_integer(unsigned(dat(j)(3*pdat_size-2-define_word_size*24 + define_word_size*i downto 3*pdat_size-define_word_size*25 + define_word_size*i ))) - 64) *
+					kernel(24) + 
+				
+					(to_integer(unsigned(dat(j)(3*pdat_size-2-define_word_size*25 + define_word_size*i downto 3*pdat_size-define_word_size*26 + define_word_size*i ))) - 64) *
+					kernel(25) + 			
+
+					(to_integer(unsigned(dat(j)(3*pdat_size-2-define_word_size*26 + define_word_size*i downto 3*pdat_size-define_word_size*27 + define_word_size*i ))) - 64) *
+					kernel(26);
+			
+	
+
+				
+			end loop;
+		end if;
+	end loop;
+end process;
+
+end rtl;
--- rtl/surface_trigger.vhd
+++ rtl/surface_trigger.vhd
@@ -93,6 +93,8 @@ port(
 end component;
 -- signal definitions
 signal internal_vpp_threshold : std_logic_vector(7 downto 0);
+signal internal_vpp_threshold_verified : std_logic_vector(7 downto 0);
+
 signal internal_trig_window_length : std_logic_vector(7 downto 0);
 signal internal_trig_mask : std_logic_vector(surface_channels-1 downto 0);
 signal internal_trig_min_abv_thresh : std_logic_vector(2 downto 0);
@@ -181,14 +183,14 @@ port map(
 		clkA				=> clk_iface_i,
 		clkB				=> clk_i,
 		SignalIn_clkA	=> reg_i(47)(4), 
-		SignalOut_clkB	=> internal_require_pol_check_trigger);
+		SignalOut_clkB	=> internal_require_pol_check_trigger); --//no longer processing this 4/11/19
 HpolPowerThreshold	:	 for i in 0 to 23 generate	
 	xHPOL_THRESH: signal_sync
 	port map(
 		clkA				=> clk_iface_i,
 		clkB				=> clk_i,
 		SignalIn_clkA	=> reg_i(102)(i), 
-		SignalOut_clkB	=> internal_hpol_pow_thresh(i));
+		SignalOut_clkB	=> internal_hpol_pow_thresh(i)); --//no longer processing this 4/11/19
 end generate;
 xENABLE : signal_sync
 	port map(
@@ -232,7 +234,8 @@ begin
 end process;
 
 --//testing purposes only. Puts filtered data to the RAM to check filtering procedure
-proc_assign_temp_data : process(clk_i)
+--//plus, prevent over-ranging the threshold
+proc_assign_temp_data : process(clk_i, internal_vpp_threshold)
 begin
 	if rising_edge(clk_i) then
 		temp_data_o(0) <= surface_data_i(0);
@@ -243,18 +246,24 @@ begin
 		temp_data_o(5) <= buf_data_2(3);
 		temp_data_o(6) <= buf_data_2(4);
 		temp_data_o(7) <= buf_data_2(5);
+		--//added an upper limit to threshold on 4/16/2019:
+		if internal_vpp_threshold >= x"3E" then
+			internal_vpp_threshold_verified <= x"3E";
+		else
+			internal_vpp_threshold_verified <= internal_vpp_threshold;
+		end if;
 	end if;
 end process;
 
 --------------//
-proc_trigger_bits: process(rst_i, clk_i, dat, internal_vpp_threshold, internal_trig_mask, trig_clear)
+proc_trigger_bits: process(rst_i, clk_i, dat, internal_vpp_threshold_verified, internal_trig_mask, trig_clear)
 begin
 	for i in 0 to surface_channels-1 loop
 		if rst_i = '1' or ENABLE_SURFACE_TRIGGER = '0' then
 			--internal_vpp(i) <= 0;
 			internal_trigger_bits(i) <= '0';
 			flag_hi(i) <= '0';
-			flag_lo(i) <= '0';
+			flag_lo(i) <= '0';					
 			internal_trigger_bit_state (i) <= idle_st;
 			
 			for j in 0 to 4*define_serdes_factor-1 loop
@@ -264,12 +273,13 @@ begin
 			
 		elsif rising_edge(clk_i) then
 			---
-			
 			case internal_trigger_bit_state(i) is
+				
 				when idle_st=>
 					internal_trigger_bits(i) <= '0';
 					internal_trigger_bit_counter(i) <= (others=>'0');
-					
+						
+					--//trigger condition requires both above and below threshold + mask bit = 1 
 					if flag_hi(i) = '1' and flag_lo(i) = '1'	and internal_trig_mask(i) = '1' then
 						internal_trigger_bit_state(i) <= trig_st;
 					else
@@ -290,14 +300,16 @@ begin
 			flag_hi(i) <= vector_or(sample_flag_hi(i));
 			flag_lo(i) <= vector_or(sample_flag_lo(i));
 				
+			--//basically, this checks if signal is above/below threshold in a 2*1/(93.75MHz)~21 ns pulse window,
+			--// that overlaps every 11.5 ns
 			for j in 0 to 4*define_serdes_factor-1 loop
-				-- 0x3F is the baseline
-				if dat(i)((j+1)*define_word_size-1 downto j*define_word_size) > ((internal_vpp_threshold) + x"3F") then
+				-- 0x3F is ~the baseline
+				if dat(i)((j+1)*define_word_size-1 downto j*define_word_size) > (x"3F" + (internal_vpp_threshold_verified)) then
 					sample_flag_hi(i)(j) <= '1';
 				else
 					sample_flag_hi(i)(j) <= '0';
 				end if;
-				if dat(i)((j+1)*define_word_size-1 downto j*define_word_size) < ((internal_vpp_threshold) + x"3F") then
+				if dat(i)((j+1)*define_word_size-1 downto j*define_word_size) < (x"3F" - (internal_vpp_threshold_verified)) then
 					sample_flag_lo(i)(j) <= '1';
 				else
 					sample_flag_lo(i)(j) <= '0';
@@ -310,58 +322,60 @@ begin
 	end loop;
 end process;
 --------------//
+--//commented out 4/11/19
+--
 --//calculate power in Hpol and Vpol channels separately, w/ option to be used as a trigger condition
-proc_wfm_power : process(rst_i, clk_i)
-begin
-	if rst_i = '1' then
-		for i in 0 to surface_channels-1 loop
-			instantaneous_avg_power(i) <= (others=>'0');
-			buffered_powersum(i) 		<= (others=>'0');
-		end loop;
-		
-		hpol_power <= (others=>'0');		
-		vpol_power <= (others=>'0');
-		internal_hpol_trig_satisfied <= '0';
-		
-	elsif rising_edge(clk_i) then
-		
-		if (hpol_power > vpol_power) and (hpol_power > internal_hpol_pow_thresh) then
-			internal_hpol_trig_satisfied <= '1';
-		else
-			internal_hpol_trig_satisfied <= '0';
-		end if;
-		
-		hpol_power <= 	std_logic_vector(resize(unsigned(instantaneous_avg_power(3)), 24)) + 
-							std_logic_vector(resize(unsigned(instantaneous_avg_power(4)), 24)) + 
-							std_logic_vector(resize(unsigned(instantaneous_avg_power(5)), 24));
-		vpol_power <= 	std_logic_vector(resize(unsigned(instantaneous_avg_power(0)), 24)) + 
-							std_logic_vector(resize(unsigned(instantaneous_avg_power(1)), 24)) + 
-							std_logic_vector(resize(unsigned(instantaneous_avg_power(2)), 24));
-							
-		for i in 0 to surface_channels-1 loop
-			--//power in 32 samples
-			instantaneous_avg_power(i) <= 
-					std_logic_vector(resize(unsigned(buffered_powersum(i)(1*(define_pow_sum_range+1)-1 downto 0)), define_16avg_pow_sum_range)) + 
-					std_logic_vector(resize(unsigned(buffered_powersum(i)(2*(define_pow_sum_range+1)-1 downto 1*(define_pow_sum_range+1))), define_16avg_pow_sum_range)) + 
-					std_logic_vector(resize(unsigned(buffered_powersum(i)(3*(define_pow_sum_range+1)-1 downto 2*(define_pow_sum_range+1))), define_16avg_pow_sum_range)) + 
-					std_logic_vector(resize(unsigned(buffered_powersum(i)(4*(define_pow_sum_range+1)-1 downto 3*(define_pow_sum_range+1))), define_16avg_pow_sum_range)) + 
-					std_logic_vector(resize(unsigned(buffered_powersum(i)(5*(define_pow_sum_range+1)-1 downto 4*(define_pow_sum_range+1))), define_16avg_pow_sum_range)) + 
-					std_logic_vector(resize(unsigned(buffered_powersum(i)(6*(define_pow_sum_range+1)-1 downto 5*(define_pow_sum_range+1))), define_16avg_pow_sum_range)) + 
-					std_logic_vector(resize(unsigned(buffered_powersum(i)(7*(define_pow_sum_range+1)-1 downto 6*(define_pow_sum_range+1))), define_16avg_pow_sum_range)) + 
-					std_logic_vector(resize(unsigned(buffered_powersum(i)(8*(define_pow_sum_range+1)-1 downto 7*(define_pow_sum_range+1))), define_16avg_pow_sum_range)) +
-					std_logic_vector(resize(unsigned(buffered_powersum(i)(9*(define_pow_sum_range+1)-1 downto 8*(define_pow_sum_range+1))), define_16avg_pow_sum_range)) + 
-					std_logic_vector(resize(unsigned(buffered_powersum(i)(10*(define_pow_sum_range+1)-1 downto 9*(define_pow_sum_range+1))), define_16avg_pow_sum_range)) + 
-					std_logic_vector(resize(unsigned(buffered_powersum(i)(11*(define_pow_sum_range+1)-1 downto 10*(define_pow_sum_range+1))), define_16avg_pow_sum_range)) + 
-					std_logic_vector(resize(unsigned(buffered_powersum(i)(12*(define_pow_sum_range+1)-1 downto 11*(define_pow_sum_range+1))), define_16avg_pow_sum_range)) +
-					std_logic_vector(resize(unsigned(buffered_powersum(i)(13*(define_pow_sum_range+1)-1 downto 12*(define_pow_sum_range+1))), define_16avg_pow_sum_range)) + 
-					std_logic_vector(resize(unsigned(buffered_powersum(i)(14*(define_pow_sum_range+1)-1 downto 13*(define_pow_sum_range+1))), define_16avg_pow_sum_range)) + 
-					std_logic_vector(resize(unsigned(buffered_powersum(i)(15*(define_pow_sum_range+1)-1 downto 14*(define_pow_sum_range+1))), define_16avg_pow_sum_range)) + 
-					std_logic_vector(resize(unsigned(buffered_powersum(i)(16*(define_pow_sum_range+1)-1 downto 15*(define_pow_sum_range+1))), define_16avg_pow_sum_range));	
-			--//double the vector length so we can add 32 samples
-			buffered_powersum(i) <= buffered_powersum(i)(define_num_power_sums*(define_pow_sum_range+1)-1 downto 0) & surface_wfm_pow(i);	
-		end loop;
-	end if;
-end process;
+--proc_wfm_power : process(rst_i, clk_i)
+--begin
+--	if rst_i = '1' then
+--		for i in 0 to surface_channels-1 loop
+--			instantaneous_avg_power(i) <= (others=>'0');
+--			buffered_powersum(i) 		<= (others=>'0');
+--		end loop;
+--		
+--		hpol_power <= (others=>'0');		
+--		vpol_power <= (others=>'0');
+--		internal_hpol_trig_satisfied <= '0';
+--		
+--	elsif rising_edge(clk_i) then
+--		
+--		if (hpol_power > vpol_power) and (hpol_power > internal_hpol_pow_thresh) then
+--			internal_hpol_trig_satisfied <= '1';
+--		else
+--			internal_hpol_trig_satisfied <= '0';
+--		end if;
+--		
+--		hpol_power <= 	std_logic_vector(resize(unsigned(instantaneous_avg_power(3)), 24)) + 
+--							std_logic_vector(resize(unsigned(instantaneous_avg_power(4)), 24)) + 
+--							std_logic_vector(resize(unsigned(instantaneous_avg_power(5)), 24));
+--		vpol_power <= 	std_logic_vector(resize(unsigned(instantaneous_avg_power(0)), 24)) + 
+--							std_logic_vector(resize(unsigned(instantaneous_avg_power(1)), 24)) + 
+--							std_logic_vector(resize(unsigned(instantaneous_avg_power(2)), 24));
+--							
+--		for i in 0 to surface_channels-1 loop
+--			--//power in 32 samples
+--			instantaneous_avg_power(i) <= 
+--					std_logic_vector(resize(unsigned(buffered_powersum(i)(1*(define_pow_sum_range+1)-1 downto 0)), define_16avg_pow_sum_range)) + 
+--					std_logic_vector(resize(unsigned(buffered_powersum(i)(2*(define_pow_sum_range+1)-1 downto 1*(define_pow_sum_range+1))), define_16avg_pow_sum_range)) + 
+--					std_logic_vector(resize(unsigned(buffered_powersum(i)(3*(define_pow_sum_range+1)-1 downto 2*(define_pow_sum_range+1))), define_16avg_pow_sum_range)) + 
+--					std_logic_vector(resize(unsigned(buffered_powersum(i)(4*(define_pow_sum_range+1)-1 downto 3*(define_pow_sum_range+1))), define_16avg_pow_sum_range)) + 
+--					std_logic_vector(resize(unsigned(buffered_powersum(i)(5*(define_pow_sum_range+1)-1 downto 4*(define_pow_sum_range+1))), define_16avg_pow_sum_range)) + 
+--					std_logic_vector(resize(unsigned(buffered_powersum(i)(6*(define_pow_sum_range+1)-1 downto 5*(define_pow_sum_range+1))), define_16avg_pow_sum_range)) + 
+--					std_logic_vector(resize(unsigned(buffered_powersum(i)(7*(define_pow_sum_range+1)-1 downto 6*(define_pow_sum_range+1))), define_16avg_pow_sum_range)) + 
+--					std_logic_vector(resize(unsigned(buffered_powersum(i)(8*(define_pow_sum_range+1)-1 downto 7*(define_pow_sum_range+1))), define_16avg_pow_sum_range)) +
+--					std_logic_vector(resize(unsigned(buffered_powersum(i)(9*(define_pow_sum_range+1)-1 downto 8*(define_pow_sum_range+1))), define_16avg_pow_sum_range)) + 
+--					std_logic_vector(resize(unsigned(buffered_powersum(i)(10*(define_pow_sum_range+1)-1 downto 9*(define_pow_sum_range+1))), define_16avg_pow_sum_range)) + 
+--					std_logic_vector(resize(unsigned(buffered_powersum(i)(11*(define_pow_sum_range+1)-1 downto 10*(define_pow_sum_range+1))), define_16avg_pow_sum_range)) + 
+--					std_logic_vector(resize(unsigned(buffered_powersum(i)(12*(define_pow_sum_range+1)-1 downto 11*(define_pow_sum_range+1))), define_16avg_pow_sum_range)) +
+--					std_logic_vector(resize(unsigned(buffered_powersum(i)(13*(define_pow_sum_range+1)-1 downto 12*(define_pow_sum_range+1))), define_16avg_pow_sum_range)) + 
+--					std_logic_vector(resize(unsigned(buffered_powersum(i)(14*(define_pow_sum_range+1)-1 downto 13*(define_pow_sum_range+1))), define_16avg_pow_sum_range)) + 
+--					std_logic_vector(resize(unsigned(buffered_powersum(i)(15*(define_pow_sum_range+1)-1 downto 14*(define_pow_sum_range+1))), define_16avg_pow_sum_range)) + 
+--					std_logic_vector(resize(unsigned(buffered_powersum(i)(16*(define_pow_sum_range+1)-1 downto 15*(define_pow_sum_range+1))), define_16avg_pow_sum_range));	
+--			--//double the vector length so we can add 32 samples
+--			buffered_powersum(i) <= buffered_powersum(i)(define_num_power_sums*(define_pow_sum_range+1)-1 downto 0) & surface_wfm_pow(i);	
+--		end loop;
+--	end if;
+--end process;
 
 -----------//
 prog_gen_trig: process(rst_i, clk_i, internal_trigger_bits, internal_trig_min_abv_thresh)
@@ -406,21 +420,25 @@ begin
 				surface_trig_state <= idle_st;
 			-------------
 			when check_pol_st =>
-				internal_trigger_counter <=internal_trigger_counter + 1;
-				trig <= '0';
-				trig_clear <= '0';
-				--if pol check is disabled:
-				if internal_require_pol_check_trigger = '0' then
-					surface_trig_state <= trig_st;
-				--hpol>vpol satisfied:
-				elsif internal_hpol_trig_satisfied = '1' then
-					surface_trig_state <= trig_st;
-				--timeout, wait 10 clock cycles (~100ns):
-				elsif internal_trigger_counter > 9 then
-					surface_trig_state <= clear_st;
-				else
-					surface_trig_state <= check_pol_st;
-				end if;
+				surface_trig_state <= trig_st;
+				
+-- commented out 4/11/19
+--				internal_trigger_counter <=internal_trigger_counter + 1;
+--				trig <= '0';
+--				trig_clear <= '0';
+--
+--				--if pol check is disabled:
+--				if internal_require_pol_check_trigger = '0' then
+--					surface_trig_state <= trig_st;
+--				--hpol>vpol satisfied:
+--				elsif internal_hpol_trig_satisfied = '1' then
+--					surface_trig_state <= trig_st;
+--				--timeout, wait 10 clock cycles (~100ns):
+--				elsif internal_trigger_counter > 9 then
+--					surface_trig_state <= clear_st;
+--				else
+--					surface_trig_state <= check_pol_st;
+--				end if;
 			-------------
 			when trig_st=>
 				internal_trigger_counter <= (others=>'0');
@@ -456,7 +474,7 @@ SurfacePower	:	 for i in 0 to surface_channels-1 generate
 			sum_pow_o=> surface_wfm_pow(i));
 end generate;
 
-xFILTER : entity work.filter
+xFILTER : entity work.filter_v2
 port map(
 		rst_i			    =>	rst_i,
 		clk_i			    =>  	clk_i,
